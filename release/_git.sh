c3d041460ae14ccec7dc130a11e34652153dc099
nikki-pru <nikki.prudencio@liferay.com>
2026-02-17 20:57:50 -0800

    LPD-78902 Testing temp release-2026.q1 branch for licensing

--- release/_git.sh
+++ release/_git.sh
@@ -242,7 +242,8 @@ function update_release_tool_repository {
 
 	git reset --hard && git clean -dfx
 
-	local release_tool_sha=$(lc_get_property "${_PROJECTS_DIR}"/liferay-portal-ee/release.properties "release.tool.sha")
+	#local release_tool_sha=$(lc_get_property "${_PROJECTS_DIR}"/liferay-portal-ee/release.properties "release.tool.sha")
+	local release_tool_sha="6f84f82e1fa9738bc8a6b8cac935de0f7471c148"
 
 	if [ ! -n "${release_tool_sha}" ]
 	then
@@ -259,13 +260,23 @@ function update_release_tool_repository {
 		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	fi
 
-	git fetch --force --prune upstream
+	# git fetch --force --prune upstream
 
-	git fetch --force --prune --tags upstream
+	# git fetch --force --prune --tags upstream
 
-	git checkout master
+	# git checkout master
 
-	git pull upstream master
+	# git pull upstream master
+
+	# git remote add tinatian git@github.com:tinatian/liferay-release-tool-ee.git
+
+	git fetch --force --prune tinatian
+
+	git fetch --force --prune --tags tinatian
+
+	git checkout LPD-79736-1
+
+	git pull tinatian LPD-79736-1
 
 	git checkout "${release_tool_sha}"
 
